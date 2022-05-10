08058d8712d5a25131af0ac0dba00c6886d179b3
yousuf18 <yousufahmedpp@gmail.com>
2022-05-10 21:56:09 +0600

    Test1

--- AdmissionTest3/Task1/Program.cs
+++ AdmissionTest3/Task1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Task1;
+
+var text = Console.ReadLine();
+
+VowelCounter vowelCounter = new VowelCounter();
+if (string.IsNullOrEmpty(text))
+    return;
+var result = vowelCounter.GetCounts(text);
+
+result = result.Substring(0, result.Length - 1);
+Console.WriteLine(result);
--- AdmissionTest3/Task1/VowelCounter.cs
+++ AdmissionTest3/Task1/VowelCounter.cs
@@ -10,7 +10,38 @@ namespace Task1
     {
         public string GetCounts(string text)
         {
-            throw new NotImplementedException();
+            int[] arr = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in text)
+            {
+
+                int ascii = item;
+                if ((ascii == 65||  ascii == 69||ascii==73||ascii==79||ascii==85) || (ascii == 97 || ascii == 101||ascii==105||ascii==111||ascii==117))
+                {
+                    if (ascii >= 97 && ascii <= 122)
+                        ascii -= 32;
+                    arr[ascii - 65]++;
+
+                }
+            }
+            sb.Clear();
+            for (int i = 0; i < 26; i++)
+            {
+                if (i == 0||i==4||i==8||i==14||i==20)
+                {
+                    int ch = i + 65 + 32;
+
+                    sb.Append((char)ch);
+                    sb.Append('-');
+                    sb.Append(arr[i]);
+                    sb.Append(',');
+
+
+                }
+
+            }
+            return sb.ToString();
         }
     }
 }
