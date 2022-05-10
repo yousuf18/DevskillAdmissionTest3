a8deb716ecb5be9123cc666f6ed6197ceb2657aa
yousuf18 <yousufahmedpp@gmail.com>
2022-05-10 22:34:35 +0600

    Task 3

--- AdmissionTest3/Task3/PrimeFinder.cs
+++ AdmissionTest3/Task3/PrimeFinder.cs
@@ -10,7 +10,36 @@ namespace Task3
     {
         public int[] GetPrimes(int lowerValue, int upperValue)
         {
-            throw new NotImplementedException();
+            int j = 0;
+            int[] arr1 = new int[upperValue - lowerValue+1];
+            while (lowerValue < upperValue)
+            {
+               
+                int i,flag;
+                flag = 0;
+                for (i = 2; i <= lowerValue / 2; i++)
+                {
+                    if (lowerValue % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+               
+               
+                if (flag == 0)
+                { 
+                    arr1[j]= lowerValue;
+                    j++;
+                }
+                lowerValue++;
+
+
+            }
+            return arr1;
+          
+            
         }
     }
+    
 }
--- AdmissionTest3/Task3/Program.cs
+++ AdmissionTest3/Task3/Program.cs
@@ -1,8 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Task3;
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Input Lower Value");
+int lowervalue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Upper Value");
+int Uppervalue = Convert.ToInt32(Console.ReadLine());
 PrimeFinder primeFinder = new PrimeFinder();
-var actualResult = primeFinder.GetPrimes(10000, 20000);
-foreach(var result in actualResult)
+var actualResult = primeFinder.GetPrimes(lowervalue, Uppervalue);
+foreach (var result in actualResult)
+{
+    if (result == 0)
+        return;
     Console.Write($"{result}, ");
+}
